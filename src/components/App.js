0d10ee3964b9ae9012f92d25843126002f7aa03c
Abhijeet <dev1.parsa@gmail.com>
2020-06-21 22:46:46 -0400

    map props with state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import SongsList from './SongsList'
 
-
-const App = ()=>{
-    return <div>App</div>
+const App = () => {
+    return (
+    <div className="ui container grid" style={{marginTop : '2px'}}>
+        <div className="ui row">
+        <div className="column eight wide">
+        <SongsList />
+        </div>
+        </div>
+    </div>)
 }
 
 export default App
